3a78294482a7f8611a8fcb09d561dc9d4b7be62f
Michael Guedko <mjgeko@gmail.com>
2022-03-13 20:41:18 -0400

    Add proper button toggling
        -Changes the read value in myLibrary object
        -Changes button value; Still need to implement styling

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const libraryGrid = document.getElementById('library');
+document.get
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -12,6 +13,10 @@ Book.prototype.info = function () {
     this.read ? x = 'Has read' : x = 'Not yet read';
     return (`<h3>${this.title}</h3> <p>by ${this.author}</p> <p>${this.pages} pages</p> <button id="btn${myLibrary.indexOf(this)}">${x}</button>`);
 }
+Book.prototype.readToggle = function () {
+    this.read = !this.read;
+    return(this.read);
+}
 
 function addBookToLibrary() {
     //This will be called when user presses submit button on for with new book info
@@ -21,7 +26,7 @@ function displayLibrary() {
     //Loop through myLibrary[] and add book cards to html
     myLibrary.forEach(book => {
         const div = document.createElement('div');
-        const bookID = 'book'+ myLibrary.indexOf(book);
+        const bookID = 'book' + myLibrary.indexOf(book);
         div.className = 'book-item';
         div.id = bookID
         div.innerHTML = `${book.info()}
@@ -33,7 +38,17 @@ function displayLibrary() {
             </div>
         `;
         libraryGrid.appendChild(div);
+        let btn = document.getElementById('btn' + myLibrary.indexOf(book));
+        btn.addEventListener('click',readStatus);
     });
+
+}
+function readStatus(event){
+    let bookIndex = event.target.id
+    bookIndex = bookIndex.replace(/\D/g,'');
+    if(myLibrary[bookIndex].readToggle()) event.target.innerHTML = 'Has read';
+    else event.target.innerHTML = 'Not yet read';
+    
 }
 const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 myLibrary.push(book1);
